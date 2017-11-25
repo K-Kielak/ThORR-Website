a9b5fba9c2cd22ec1e393c2831e059da4093d0f8
Joe R <joseph.m.ramage@baesystems.com>
2017-11-25 18:52:08 +0000

    Added display of overspend.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Navbar, NavItem, Row, Col, Container, CardPanel } from 'react-materialize';
+import { Navbar, NavItem, Row, Col, Container, CardPanel, Card } from 'react-materialize';
 
 import { PieChart, Pie, Cell } from 'recharts';
 const COLORS = ['#F44336', '#FFC107', '#4CAF50']
@@ -13,7 +13,7 @@ class Home extends Component {
         {name:"Amber", uv:5},
         {name:"Green", uv:90}
         ],
-      "spend":-100
+      "spend":15000
     };
   }
 
@@ -22,10 +22,42 @@ class Home extends Component {
   }
 
   formatMoney(val){
-    return "£"+Math.abs(val);
+    var number = Math.abs(val)
+    var digits = Math.log(number) * Math.LOG10E + 1 | 0;
+    var formattedNumber = number;
+    switch (digits) {
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+        console.log(digits + " is either 0,1,2,3");
+        formattedNumber = number;
+        break;
+      case 4:
+      case 5:
+      case 6:
+        console.log(digits + " is either 4,5,6");
+        formattedNumber = Math.round(number/1000)
+        formattedNumber += "k"
+        break;
+      case 7:
+      case 8:
+      case 9:
+        console.log(digits + " is either 7,8,9,10,11,12");
+        formattedNumber = Math.round(number/100000)/10
+        formattedNumber += "m"
+        break;
+
+      default:
+        console.log(digits + " is bigger than 12");
+        formattedNumber = Math.round(number/100000000)/10
+        formattedNumber += "bn"
+        break;
+    }
+    return "£"+ formattedNumber;
   }
   get_overspend(){
-    return this.state.spend > 0 ? <div className="overspend">{this.formatMoney(this.state.spend)}</div> : <div className="underspend">{"-" + this.formatMoney(this.state.spend)}</div>;
+    return this.state.spend > 0 ? <div className="overspend"><h2>{this.formatMoney(this.state.spend)}</h2></div> : <div className="underspend"><h2>{"-" + this.formatMoney(this.state.spend)}</h2></div>;
   }
   render(){
         return (
@@ -44,12 +76,9 @@ class Home extends Component {
                   </CardPanel>
                 </Col>
                 <Col s={12} m={4}>
-                  <CardPanel>
-                    <h1>Budget</h1>
-                  </CardPanel>
-                  <CardPanel>
+                  <Card title="Total Overspend/Underspend">
                     {this.get_overspend()}
-                  </CardPanel>
+                  </Card>
                 </Col>
             </Row>
           </div>
