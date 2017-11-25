00b816e215f315ddbe2dab5457f5f7938ce2e336
Joe R <joseph.m.ramage@baesystems.com>
2017-11-25 17:58:35 +0000

    Added clickable pie chart.

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,15 +1,57 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Navbar, NavItem } from 'react-materialize';
+import { Navbar, NavItem, Row, Col, Container, CardPanel } from 'react-materialize';
 
+import { PieChart, Pie, Cell } from 'recharts';
+const COLORS = ['#F44336', '#FFC107', '#4CAF50']
 class Home extends Component {
-    render(){
+  constructor(props){
+    super(props);
+    this.state = {
+      "pie_chart_data": [
+        {name:"Red", uv:5 },
+        {name:"Amber", uv:5},
+        {name:"Green", uv:90}
+        ],
+      "spend":-100
+    };
+  }
+
+  pieClick(info){
+    console.log("CLICK THAT PIE" + JSON.stringify(info.activePayload[0].payload.name));
+  }
+
+  formatMoney(val){
+    return "£"+Math.abs(val);
+  }
+  get_overspend(){
+    return this.state.spend > 0 ? <div className="overspend">{this.formatMoney(this.state.spend)}</div> : <div className="underspend">{"-" + this.formatMoney(this.state.spend)}</div>;
+  }
+  render(){
         return (
           <div>
             <Navbar>Test</Navbar>
-            <div>
-              <p>Test Paragraph</p>
-            </div>
+              <Row>
+                <Col s={12} m={4}>
+                  <CardPanel className="teal lighten-4 black-text">
+                    <PieChart width={400} height={400} onClick={this.pieClick}>
+                      <Pie type="monotone" data={this.state.pie_chart_data} dataKey="uv">
+                        {
+                        	this.state.pie_chart_data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+                        }
+                      </Pie>
+                    </PieChart>
+                  </CardPanel>
+                </Col>
+                <Col s={12} m={4}>
+                  <CardPanel>
+                    <h1>Budget</h1>
+                  </CardPanel>
+                  <CardPanel>
+                    {this.get_overspend()}
+                  </CardPanel>
+                </Col>
+            </Row>
           </div>
         );
     }
